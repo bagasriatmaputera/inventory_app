0a1e65a97e6c8f7626569df7fde5f7d09c6896f5
Bagas Riatma Putera <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-10-01 19:01:53 +0700

    add warehouse repository

---
+++ app/Repository/WarehouseRepositories.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Repository;
+
+use App\Models\Category;
+use App\Models\Warehouse;
+
+class WarehouseRepositories {
+    public function getAll(array $fields){
+        return Warehouse::with(['products.category'])->latest()->paginate(10);
+    }
+    public function getById(int $id, array $fields){
+        return Warehouse::with(['products.category'])->findOrFail($id);
+    }
+    public function create(array $data){
+        return Warehouse::create($data);
+    }
+    public function update(int $id, array $data){
+        $warehouse = Warehouse::findOrFail($id);
+        $warehouse->update($data);
+        return $warehouse;
+    }
+    public function delete(int $id)
+    {
+        $warehouse = Warehouse::findOrFail($id);
+        $warehouse->delete();
+    }
+}
