12c4174efbb63cc34b0a6d33e042d260b096e952
Bagas Riatma Putera <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-10-10 20:02:59 +0700

    add MerchantProduct Repo

---
+++ app/Repository/MerchantProductRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\MerchantProduct;
+use Illuminate\Validation\ValidationException;
+use PhpParser\Node\Expr\Throw_;
+
+class MerchantProductRepository
+{
+    public function create(array $data)
+    {
+        return MerchantProduct::create($data);
+    }
+    public function getByMerchantProduct(int $merchantId, int $productId)
+    {
+        return MerchantProduct::where('merchat_id', $merchantId)
+            ->where('product_id', $productId)
+            ->first();
+    }
+    public function updateStock(int $stock, int $merchantId, $productId)
+    {
+        $merchant = $this->getByMerchantProduct($merchantId, $productId);
+        if (!$merchant) {
+            throw ValidationException::withMessages([
+                'product_id' => 'Product not found for this merchant'
+            ]);
+        }
+        $merchant->update(['stock' => $stock]);
+        return $merchant;
+    }
+}
