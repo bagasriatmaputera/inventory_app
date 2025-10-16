05ef8e568c51eb03ef52213522d4f74aed20c925
Bagas Riatma Putera <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-10-16 23:03:51 +0700

    add UserResource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Merchant;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'photo' => $this->photo,
+            'roles' => $this->roles->pluck('name'),
+            'merchant' => new MerchantResource($this->merchant)
+        ];
+    }
+}
