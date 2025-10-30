3f5696209ce86d9c7ac33e8695cff70a2d1f460b
Bagas Riatma Putera <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-10-30 19:13:55 +0700

    add route api

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MerchantController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\WarehouseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('products', ProductController::class);
+Route::apiResource('warehouses', WarehouseController::class);
+Route::apiResource('merchants', MerchantController::class);
